80a00d4e9f11689604f7d68dc7b0a1d768faa143
sandeepchaudhar <119068999+sandeepchaudhar@users.noreply.github.com>
2022-12-01 23:16:00 +0530

    Create excer5.py

---
+++ excer5.py
@@ -0,0 +1,11 @@
+name = input("please enter your name")
+# harshit vashita
+#harshit , len - 1 = 6
+#name.count("h") , name.count(name[0])  = 2
+#name.count("a") , name.count(name[1])  = 1
+#name.count("r") , name.count(name[2])  = 1
+#name.count("s") , name.count(name[3])  = 1
+#name.count("h") , name.count(name[4])  = 2
+#name.count("i") , name.count(name[5])  = 1
+#name.count("t") , name.count(name[6])  = 1
+#  output
